fb1f226980cc7d192484612f8da88af3e3d35772
Abhishek Patel <64314222+imdarkcoder@users.noreply.github.com>
2022-01-01 21:41:30 +0530

    Update Image.jsx

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -3,10 +3,12 @@ import { makeStyles } from '@material-ui/core';
 const useStyles = makeStyles({
     image: {
         height: 300,
-        width: '100%',
+        width: '96%',
         objectFit:'cover',
         borderRadius: 20,
-        backgroundColor: 'black'
+        backgroundColor: 'black',
+        boxShadow: '0px 0px 10px #30373D',
+        padding: 5,
     }
     
 })
